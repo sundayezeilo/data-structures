04619555a463f16f9eb608b870a509ac7cb008d7
Sunday Ezeilo <ezeilo.su@gmail.com>
2022-08-28 22:43:50 +0100

    Implement Queue with tests

--- src/__test__/queue.spec.ts
+++ src/__test__/queue.spec.ts
@@ -5,10 +5,10 @@ describe('Queue', () => {
     it('should add a value to the queue', () => {
       const queue = new Queue();
 
-      const newSize = queue.enqueue(3);
+      const newSize = queue.enqueue('FIRST');
 
       expect(newSize).toEqual(1);
-      expect(queue.first!.value).toBe(3);
+      expect(queue.first!.value).toBe('FIRST');
       expect(queue.first).toMatchObject({ ...queue.last });
     });
 
@@ -21,11 +21,11 @@ describe('Queue', () => {
     it('should add more than one values to the queue', () => {
       const queue = new Queue();
 
-      queue.enqueue(7);
-      queue.enqueue(100);
-      queue.enqueue(5000);
-      expect(queue.first!.value).toEqual(7);
-      expect(queue.first?.next?.value).toEqual(100);
+      queue.enqueue('FIRST');
+      queue.enqueue('SECOND');
+      queue.enqueue('THIRD');
+      expect(queue.first!.value).toEqual('FIRST');
+      expect(queue.first?.next?.value).toEqual('SECOND');
     });
   });
 
@@ -33,29 +33,34 @@ describe('Queue', () => {
     it('should remove a value from the queue', () => {
       const queue = new Queue();
 
-      queue.enqueue(7);
-      queue.enqueue(100);
-      queue.enqueue(300);
-      queue.enqueue(5000);
-      queue.enqueue(1000);
+      queue.enqueue('FIRST');
+      queue.enqueue('SECOND');
+      queue.enqueue('THIRD');
+      queue.enqueue('FOURTH');
+      queue.enqueue('FIFTH');
 
       const oldSize = queue.size;
       queue.dequeue();
       queue.dequeue();
 
       expect(queue.size).toEqual(oldSize - 2);
-      expect(queue.first!.value).toBe(300);
+      expect(queue.first!.value).toBe('THIRD');
     });
 
     it('should return the correct value by calling dequeue', () => {
       const queue = new Queue();
-      queue.enqueue(7);
-      queue.enqueue(2);
-      queue.enqueue(30);
+      
+      queue.enqueue('FIRST');
+      queue.enqueue('SECOND');
+      queue.enqueue('THIRD');
+      queue.enqueue('FOURTH');
+      queue.enqueue('FIFTH');
 
-      expect(queue.dequeue()).toEqual(7);
-      expect(queue.dequeue()).toEqual(2);
-      expect(queue.dequeue()).toEqual(30);
+      expect(queue.dequeue()).toEqual('FIRST');
+      expect(queue.dequeue()).toEqual('SECOND');
+      expect(queue.dequeue()).toEqual('THIRD');
+      expect(queue.dequeue()).toEqual('FOURTH');
+      expect(queue.dequeue()).toEqual('FIFTH');
       expect(queue.dequeue()).toBeUndefined();
     });
   });
